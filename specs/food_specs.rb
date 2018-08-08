0bf6c3b2f11752d9bf4fa996955ef4c40eae16d7
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-08 14:48:33 +0100

    food_has_price and food_has_rejuvenation_level created

--- specs/food_specs.rb
+++ specs/food_specs.rb
@@ -14,4 +14,12 @@ def test_food_has_name
   assert_equal("Pizza", @food1.name)
 end
 
+def test_food_has_price
+  assert_equal(10.30, @food1.price)
+end
+
+def test_food_has_rejuvenation_level
+  assert_equal(3, @food1.rejuvenation_level)
+end
+
 end
