32e716b3976a59104c4df2f01f24afb6b1d05373
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-08 11:39:48 +0100

    pub_has_name created

--- pub.rb
+++ pub.rb
@@ -1,2 +1,11 @@
 class Pub
+
+  attr_reader(:name, :till)
+
+  def initialize(name, till, drinks)
+    @name = name
+    @till = till
+    @drinks = []
+  end
+
 end
--- specs/pub_specs.rb
+++ specs/pub_specs.rb
@@ -1,10 +1,20 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../pub.rb')
+require_relative('../drink.rb')
 
 class PubTest < MiniTest::Test
 
+  def setup
+    @drink1 = Drink.new("Martini", 8, 2)
+    @drink2 = Drink.new("Picpoul", 6, 3)
 
+    @pub = Pub.new("SuperPub", 250, [@drink1, @drink2])
+  end
+
+  def test_pub_has_name
+    assert_equal("SuperPub", @pub.name)
+  end
 
 
 
