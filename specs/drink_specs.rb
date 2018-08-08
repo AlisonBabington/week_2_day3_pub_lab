e5e9aacf42af9b94e976600e94446d685be17902
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-08 11:19:47 +0100

    drink_has_name created

--- specs/drink_specs.rb
+++ specs/drink_specs.rb
@@ -4,7 +4,13 @@ require_relative('../drink.rb')
 
 class DrinkTest < MiniTest::Test
 
+def setup
+  @drink = Drink.new("Mojito", 8, 2)
+end
 
+def test_drink_has_name
+  assert_equal("Mojito", @drink.name)
+end
 
 
 
